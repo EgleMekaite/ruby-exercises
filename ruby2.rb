effd48c1f81dab65e82c65b25c63daf9cb0ca339
Egle <egle.mekaite@getapp.com>
2018-03-15 15:53:52 +0100

    the last version of ruby2.rb

--- ruby2.rb
+++ ruby2.rb
@@ -1,27 +1,27 @@
 puts "Please enter your status:"
-while status = gets.chomp
+while status = gets.chomp.upcase
 
     if status == "M"
         puts "You are married."
         puts "Please enter your salary:"
-        salary = gets.chomp.to_i
+        salary = gets.chomp.gsub(/[. ]/,"").to_i
         if salary <= 18000
             puts "Your tax rate is 10%"
         elsif salary <= 75000
             puts "Your tax rate is 15%"
-        elsif salary > 75000
+        else
             puts "Your tax rate is 25% and greater"
         end
         break
     elsif status == "S"
         puts "You are single."
         puts "Please enter your salary:"
-        salary = gets.chomp.to_i
+        salary = gets.chomp.gsub(/[. ]/,"").to_i
         if salary <= 9500
             puts "Your tax rate is 10%"
         elsif salary <= 38000
             puts "Your tax rate is 15%"
-        elsif salary > 38000
+        else
             puts "Your tax rate is 25% and greater"
         end
         break
